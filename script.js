48d7e067e3c492e2102dde377504bc74111bd64d
cbstorms <storms.christopher.b@gmail.com>
2024-12-11 22:34:06 +0100

    Update playRound function

--- script.js
+++ script.js
@@ -19,23 +19,31 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
 function playRound() {
-    if (getComputerChoice() === 'rock' && getHumanChoice() === 'scissors') {
+    if (computerSelection === 'rock' && humanSelection === 'scissors') {
         return "You lose :( Rock beats scissors.";
-    } else if (getComputerChoice() === 'rock' && getHumanChoice() === 'paper') {
+    } else if (computerSelection === 'rock' && humanSelection === 'paper') {
         return "You win! Paper beats rock.";
-    } else if (getComputerChoice() === 'paper' && getHumanChoice() === 'scissors') {
+    } else if (computerSelection === 'rock' && humanSelection === 'rock') {
+        return "Tie! Rock.";
+    } else if (computerSelection === 'paper' && humanSelection === 'scissors') {
         return "You win! Scissors beat paper.";
-    } else if (getComputerChoice() === 'paper' && getHumanChoice() === 'rock') {
+    } else if (computerSelection === 'paper' && humanSelection === 'rock') {
         return "You lose :( Paper beats  rock.";
-    } else if (getComputerChoice() === 'scissors' && getHumanChoice() === 'paper') {
+    } else if (computerSelection === 'paper' && humanSelection === 'paper') {
+        return "Tie! Paper.";
+    } else if (computerSelection === 'scissors' && humanSelection === 'paper') {
         return "You lose :( Scissors beat paper.";
-    } else if (getComputerChoice() === 'scissors' && getHumanChoice() === 'rock') {
+    } else if (computerSelection === 'scissors' && humanSelection === 'rock') {
         return "You win! Rock beats scissors.";
+    } else if (computerSelection === 'scissors' && humanSelection === 'scissors') {
+        return "Tie! Scissors.";
     }
 }
 
-
-//console.log(getComputerChoice());
-//console.log(getHumanChoice());
+console.log(computerSelection);
+console.log(humanSelection);
 console.log(playRound());
